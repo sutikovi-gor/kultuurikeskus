6b5e3483f35fe6ad2f00b990de2e2df06033d10b
sutikovi-gor <sutikovi@gmail.com>
2022-06-03 16:56:54 +0300

    DB_news

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+<?php
+
+//require 'dbConnect.php';
+
+?>
+
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="css/style.css">
-  <?php
-    require 'dbConnect.php';
-  ?>
+  
+    
+  
 	<title></title>
 </head>
 <body>
@@ -39,20 +46,40 @@
  <div class="global_block">
  	<p>Это первый блок</p>
 
-<?php
+
+
+  <?php
+
+$db_server='localhost';
+$db_database='kultuurnews';
+$db_user='root'; 
+$db_password='';
+
+//соединение с базой
+$connection=mysqli_connect($db_server,$db_user, $db_password, $db_database);
+mysqli_select_db($connection, "kultuurnews");
+//контроль соединения
+if (!$connection){
+  die ("Ei saa ühendust andmebaasiga");
+}
+date_default_timezone_set('Europe/Tallinn');
+
+$connection->set_charset('utf-8');
+
 
   echo '<ul>';
 
   $query = mysqli_query($connection, "SELECT * FROM news ORDER BY id DESC");
-      $numrows = mysqli_num_rows($query);
-      while ($row = mysqli_fetch_array($query)) {
+  $numrows = mysqli_num_rows($query);
+  while ($row = mysqli_fetch_array($query)) {
   echo '<option>'.$row['title'].'</option>';
   echo '<option>'.$row['text'].'</option>';
   }
 
-  echo '<ul>'
+  echo '<ul>';
 
 ?> 
+
 
  </div><!--
 
